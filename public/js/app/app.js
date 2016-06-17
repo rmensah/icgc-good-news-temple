583082541b7b7a766d264ce7ed7f2866eab4bcbc
rmensah <richiemens@gmail.com>
2016-06-17 16:02:56 -0400

    added css and made views refactoring

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,14 +1,14 @@
 
-// $( document ).ready(function(){
+$( document ).ready(function(){
 
-//  $(".dropdown-button").dropdown();
-//  $(".button-collapse").sideNav();
+ $(".dropdown-button").dropdown();
+ $(".button-collapse").sideNav();
 
-// }) 
+}) 
 
- $(document).ready(function(){
-      ('.slider').slider({full_width: true});
-      ('.dropdown-button').dropdown();
-      ('.button-collapse').sideNav();
-    });
-        
+ // $(document).ready(function(){
+ //      ('.slider').slider({full_width: true});
+ //      ('.dropdown-button').dropdown();
+ //      ('.button-collapse').sideNav();
+ //    });
+ //        
